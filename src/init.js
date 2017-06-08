f90e5761c7a610eecd4b2c7bb38ee6cac8e45151
Jason Allshorn <deepsleepsounds@gmail.com>
2017-06-09 05:15:36 +0900

    first completed pseodoclassical

--- src/init.js
+++ src/init.js
@@ -4,8 +4,8 @@ $(document).ready(function() {
   $('.addDancerButton').on('click', function(event) {
     /* This function sets up the click handlers for the create-dancer
      * buttons on dancefloor.html. You should only need to make one small change to it.
-     * As long as the "data-dancer-maker-function-name" attribute of a
-     * class="addDancerButton" DOM node matches one of the names of the
+     * As long as the 'data-dancer-maker-function-name' attribute of a
+     * class='addDancerButton' DOM node matches one of the names of the
      * maker functions available in the global scope, clicking that node
      * will call the function to make the dancer.
      */
@@ -16,18 +16,20 @@ $(document).ready(function() {
      * to the stage.
      */
     var dancerMakerFunctionName = $(this).data('dancer-maker-function-name');
-
+    console.log(dancerMakerFunctionName)
     // get the maker function for the kind of dancer we're supposed to make
     var dancerMakerFunction = window[dancerMakerFunctionName];
-
-    // make a dancer with a random position
-
-    var dancer = dancerMakerFunction(
-      $("body").height() * Math.random(),
-      $("body").width() * Math.random(),
+      console.log(window)
+      // make a dancer with a random position
+      console.log(dancerMakerFunction)
+    var dancer = new dancerMakerFunction(
+      $('body').height() * Math.random(),
+      $('body').width() * Math.random(),
       Math.random() * 1000
     );
+    console.log(dancer);
     $('body').append(dancer.$node);
-  });
-});
 
+  });
+
+});
